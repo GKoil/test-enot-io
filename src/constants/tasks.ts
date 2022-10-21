0d7d0a66f2a53b258f59f80eae9f4ab8189f3d2b
Kirill <samsuu@ya.ru>
2022-10-21 15:16:31 +0500

    update tasks

--- src/constants/tasks.ts
+++ src/constants/tasks.ts
@@ -3,25 +3,64 @@ import { Task } from "@/types/task.type";
 const TASKS: Task[] = [
   {
     id: 1,
-    date: "10.11.2022",
-    title: "Call",
-    description: "Call my mom",
-    priority: "medium",
+    date: "10.21.2022",
+    title: "Visit David",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    priority: "hight",
     isCompleted: false,
   },
   {
     id: 2,
-    date: "10.12.2022",
-    title: "Call",
-    description: "Call my mom",
+    date: "10.21.2022",
+    title: "Goceries For Dinner",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     priority: "medium",
     isCompleted: true,
   },
   {
     id: 3,
-    date: "10.10.2022",
-    title: "Call",
-    description: "Call my mom",
+    date: "10.22.2022",
+    title: "Fix Dad’s iPad",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    priority: "hight",
+    isCompleted: true,
+  },
+  {
+    id: 4,
+    date: "10.23.2022",
+    title: "Fix Dad’s iPad",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    priority: "medium",
+    isCompleted: true,
+  },
+  {
+    id: 5,
+    date: "10.24.2022",
+    title: "Fix Dad’s iPad",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    priority: "medium",
+    isCompleted: true,
+  },
+  {
+    id: 6,
+    date: "10.25.2022",
+    title: "Fix Dad’s iPad",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+    priority: "medium",
+    isCompleted: true,
+  },
+  {
+    id: 7,
+    date: "10.26.2022",
+    title: "Fix Dad’s iPad",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
     priority: "medium",
     isCompleted: true,
   },
