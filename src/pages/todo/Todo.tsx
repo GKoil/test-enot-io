f8ae219e036d71dcb9c6cdafd0c7a697f8b3be58
Kirill <samsuu@ya.ru>
2022-10-21 15:17:07 +0500

    refactor Todo

--- src/pages/todo/Todo.tsx
+++ src/pages/todo/Todo.tsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { Typography, Box, IconButton, CircularProgress } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
@@ -11,9 +11,24 @@ import DayTasks from "./components/DayTasks";
 import { Task } from "@/types/task.type";
 import processData from "../../packages/date/processDate";
 
+const callbackSort = ([a]: [string, unknown], [b]: [string, unknown]) =>
+  a.split(".")[1] > b.split(".")[1] ? 1 : -1;
+
+const transformTasks = (tasksFn?: Task[]) => {
+  if (!tasksFn) return {};
+
+  return tasksFn.reduce((acc, item): { [key: string]: Task[] } => {
+    if (acc[item.date]) {
+      return { ...acc, [item.date]: [...acc[item.date], item] };
+    }
+    return { ...acc, [item.date]: [item] };
+  }, {} as { [key: string]: Task[] });
+};
+
 function Todo() {
   const value = useContext(context);
   const [isOpen, setIsOpen] = useState(false);
+  const [days, setDays] = useState<[string, Task[]][]>([]);
 
   const { status } = useQuery(["tasks"], () => {
     value?.actions.addTasks(TASKS);
@@ -24,36 +39,33 @@ function Todo() {
     setIsOpen((prev) => !prev);
   };
 
-  const transformTasks = (tasks?: Task[]) => {
-    if (!tasks) return {};
+  useEffect(() => {
+    const today = new Date();
 
-    return tasks.reduce((acc, item): { [key: string]: Task[] } => {
-      if (acc[item.date]) {
-        return { ...acc, [item.date]: [...acc[item.date], item] };
-      }
-      return { ...acc, [item.date]: [item] };
-    }, {} as { [key: string]: Task[] });
-  };
+    const transformedDays = transformTasks(value?.data?.tasks);
+    const processedDays = Object.entries(transformedDays)
+      .filter(([dateTask]) => {
+        const isBeforeToday =
+          today.getDate() - new Date(dateTask).getDate() <= 0;
+        return isBeforeToday;
+      })
+      .sort(callbackSort)
+      .map(([unprocessedDate, dayTasks]) => {
+        const dayDate = new Date(unprocessedDate);
+        const formateDate = processData(dayDate, today);
+        const processedDay: [string, Task[]] = [formateDate, dayTasks];
+        return processedDay;
+      });
 
-  const currentDate = useMemo((): Date => {
-    return new Date();
-  }, []);
-
-  const processDateByList = (date: string) => {
-    const today = currentDate;
-    const constructDate = new Date(date);
-    return processData(constructDate, today);
-  };
-
-  const callbackSort = ([a]: [string, unknown], [b]: [string, unknown]) =>
-    a.split(".")[1] > b.split(".")[1] ? 1 : -1;
+    setDays(processedDays);
+  }, [status, value]);
 
   return (
     <div>
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Typography variant="h4">To Do</Typography>
-        <IconButton className={styles.todo__setting} onClick={toggleModal}>
-          <SettingsIcon style={{ color: "#F4F4F4" }} fontSize="large" />
+        <IconButton onClick={toggleModal}>
+          <SettingsIcon className={styles.todo__setting} fontSize="large" />
         </IconButton>
       </Box>
 
@@ -61,23 +73,17 @@ function Todo() {
 
       {status === "success" && (
         <ul className={styles.todo__tasks}>
-          {Object.entries(transformTasks(value?.data?.tasks))
-            .sort(callbackSort)
-            .filter(
-              ([dateTask]) =>
-                currentDate.getDate() - new Date(dateTask).getDate() <= 0,
-            )
-            .map(([dateTask, itemTasks], index) => (
-              <li key={dateTask}>
-                <Box marginBottom={4}>
-                  <DayTasks
-                    data={processDateByList(dateTask)}
-                    tasks={itemTasks}
-                    isOpenDay={index === 0}
-                  />
-                </Box>
-              </li>
-            ))}
+          {days.map(([dateTask, itemTasks], index) => (
+            <li key={dateTask}>
+              <Box marginBottom={4}>
+                <DayTasks
+                  data={dateTask}
+                  tasks={itemTasks}
+                  isOpenDay={index === 0}
+                />
+              </Box>
+            </li>
+          ))}
         </ul>
       )}
 
