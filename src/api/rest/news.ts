9b0d10d1e773622d38fb30861d811b0ff838cd46
Kirill <samsuu@ya.ru>
2022-10-23 20:34:14 +0500

    fix getNews url

--- src/api/rest/news.ts
+++ src/api/rest/news.ts
@@ -4,7 +4,7 @@ import { ArticleNews } from "../../types/articleNews.type";
 // eslint-disable-next-line import/prefer-default-export
 export const getNews = () => {
   return makeRequest<{ value: ArticleNews[] }>(
-    "https://contextualwebsearch-websearch-v1.p.rapidapi.cearchAPI?q=%3CREQUIRED%3E&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null",
+    "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI?q=%3CREQUIRED%3E&pageNumber=1&pageSize=10&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null",
     {
       "X-RapidAPI-Key": "573d6d351bmsh0f85dd072f7e49bp194e92jsn4010ddd4e665",
       "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
